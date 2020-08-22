33581a88a552bafbeca79c5df375a571709af9a3
Semisol <git@smileycreations15.com>
2020-08-22 19:05:23 +0300

    Update and rename config-example.json to config-example.js

--- config-example.js
+++ config-example.js
@@ -1,5 +1,6 @@
 {
   // Note: Do NOT keep these comments, they will cause an error and are only for the description of fields.
+  // Note: The .js extension is to make the formatting work, rename this to config.json when done.
   "port":8888, // HTTP Server port. Required.
   "discord-hook":"https://discord.com/api/webhooks/xxxxxxxxxxx/xxxxxxxxxxx", // Discord error logging hook, leave empty to not log. Optional.
   "hook-secret":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // GitHub webhook secret, used for automatic deployment. Only use type application/json hooks, and only the `push` event.
