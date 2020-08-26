18e9cad1197b0957ff7ed51882168cb247ac9227
Kenny2github <kenny2minecraft@gmail.com>
2020-08-26 19:11:25 +0800

    Fix tests (and pass them). Closes #60
    .github/workflows/pythonapp.yml, config-example.js:
    * Admin usernames must be lowercase, as API usernames are casefolded
    
    backend/server.py:
    * check_username doesn't bother checking if the username exists anymore
      to minimize Scratch API calls, because verification will fail
      in those cases anyway. Because of this, debug mode is no longer required
      for some tests.
    * Update comment in _verified to be more accurate
    * Fix: login was calling Server.get_ban instead of Database.get_ban
    * Instead of calling get_user after db.reset_token, just let db.reset_token
      do the getting automatically
    * Better document debug and debug_pass
    
    tests.py:
    + set_session sets session cookie in the requests session, since that's
      how session IDs are passed now
    + Add MANY much-needed comments
    * Make test failure messages clearer
    * Section code so that it's not a wall
    * Some debugs are off now that usernames aren't checked against Scratch
    - Remove test for Session object
    + Test Admin object, and test its values on admin and non-admin
    * Fix: testing 401 and 404 had debug_pass on
    * /session/{} tests become /session tests and use cookies instead of URL
    + PATCH /session test ensures token is different too
    * Consolidate /usage test actions into a function, to support...
    + ...a new test for /usage parameters, specifically limit and start
    * Change /usage/{} test to check entire log object's equality
    
    backend/db.py:
    * Comment some code
    + Dummy client objects now have non-empty tokens
    * get_client_info has a debug bypass now
    * reset_token now properly returns client objects in all branches.
      For debugging purposes, it returns a client with a different token.

--- config-example.js
+++ config-example.js
@@ -5,5 +5,5 @@
   "discord-hook":"https://discord.com/api/webhooks/xxxxxxxxxxx/xxxxxxxxxxx", // Discord error logging hook, leave empty to not log. Optional.
   "hook-secret":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // GitHub webhook secret, used for automatic deployment. Only use type application/json hooks, and only the `push` event.
   "name":"ScratchVerifier", // Instance owner / ID. Included in the error message from the Discord webhook.
-  "admins":["user1", "user2"] // People with access to the /admin API's and /site/admin features.
+  "admins":["user1", "user2"] // LOWERCASE usernames of people with access to the /admin API's and /site/admin features.
 }
